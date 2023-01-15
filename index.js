39b349b1b99111495908808d226f9a97b92dd733
Yael Tzruia <yaeli.rogestav@gmail.com>
2023-01-15 15:35:04 +0200

    fixing image upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
@@ -24,7 +25,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const AuthRoutes = require("./routes/auth");
 const UserRoutes = require("./routes/users");
 
+const storage = multer.diskStorage({
+  //cb = callback
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "profile-image--" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.get("/uploads", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+  res.send("uploaded");
+});
+
+app.post("/uploads", upload.single("image"), (req, res) => {
+  console.log(req, "req");
+  res.send(`http://localhost:5001/uploads/${req.file.filename}`);
+});
+
 // routes
+app.use("/uploads", express.static(`${__dirname}/uploads`));
 app.use("/auth", AuthRoutes);
 app.use("/user", UserRoutes);
 
